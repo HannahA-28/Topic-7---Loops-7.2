5d4af6ed21bc2d8db0b6d7c35bdcd44194a85cf7
Hannah Anderson <339729345@GoTVDSB.ca>
2025-04-01 13:39:56 -0400

    Add project files.

---
+++ Topic 7 - Loops-7.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7___Loops_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
