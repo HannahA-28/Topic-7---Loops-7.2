0b70c1ddb98880c207bab0f0029dad77b72959fc
Hannah Anderson <339729345@GoTVDSB.ca>
2025-04-01 14:42:16 -0400

    Onto task three!

--- Topic 7 - Loops-7.2/Program.cs
+++ Topic 7 - Loops-7.2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Type a message, and I'll display it ten times");
+            //Console.WriteLine("Message: ");
+            //String message = Console.ReadLine();
+            //for (int i = 1; i <= 10; i = i + 1)
+            //    Console.WriteLine(i + ". " + message);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Task one");
+            //Console.WriteLine("What is your favourite food?");
+            //Console.WriteLine("Message: ");
+            //string message = Console.ReadLine();
+            //for (int i = 1; i <= 10; i = i + 1)
+            //    Console.WriteLine(i + ". " + message + " is delicious.");
+            //Console.ReadLine();
+
+            //Console.WriteLine("Task two");
+            //int number;
+            //Console.WriteLine("What number do you want me to count to?");
+            //Console.WriteLine("Number: ");
+            //number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine();
+            //for (int i = 0; i <= number; i += 1)
+            //    Console.Write(i + " ");
+
+            Console.WriteLine("Task three");
+
         }
     }
 }
